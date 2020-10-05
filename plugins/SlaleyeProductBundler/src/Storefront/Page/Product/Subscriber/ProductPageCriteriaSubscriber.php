139b91e2533a930432c0583d3f147109ee545f4a
sal <salomonlaleye@gmail.com>
2020-10-05 12:00:44 +0200

    add product bundle to storefront

---
+++ plugins/SlaleyeProductBundler/src/Storefront/Page/Product/Subscriber/ProductPageCriteriaSubscriber.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Slaleye\ProductBundler\Storefront\Page\Product\Subscriber;
+
+use Shopware\Storefront\Page\Product\ProductLoaderCriteriaEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ProductPageCriteriaSubscriber implements EventSubscriberInterface
+{
+
+    public static function getSubscribedEvents()
+    {
+        // TODO: Implement getSubscribedEvents() method.
+        return [
+            ProductLoaderCriteriaEvent::class => 'onProductCriteriaLoaded'
+        ];
+    }
+
+    public function onProductCriteriaLoaded(ProductLoaderCriteriaEvent $event): void
+    {
+        $event->getCriteria()->addAssociation('bundles.products.cover');
+    }
+}
