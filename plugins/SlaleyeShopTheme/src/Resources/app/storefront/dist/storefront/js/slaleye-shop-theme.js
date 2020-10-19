eff1c37dcc34f57e0d31b26ed499bb0fb9af07b9
sal <salomonlaleye@gmail.com>
2020-10-19 14:19:23 +0200

    Extend listing and add Theme config

--- plugins/SlaleyeShopTheme/src/Resources/app/storefront/dist/storefront/js/slaleye-shop-theme.js
+++ plugins/SlaleyeShopTheme/src/Resources/app/storefront/dist/storefront/js/slaleye-shop-theme.js
@@ -1,15 +1,14 @@
-(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["slaleye-shop-theme"], {
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["slaleye-shop-theme"],{
 
-    /***/ "Ckmy":
-    /*!*************************************************************************************!*\
-      !*** /app/custom/plugins/SlaleyeShopTheme/src/Resources/app/storefront/src/main.js ***!
-      \*************************************************************************************/
-    /*! no static exports found */
-    /***/ (function (module, exports) {
+/***/ "Ckmy":
+/*!*************************************************************************************!*\
+  !*** /app/custom/plugins/SlaleyeShopTheme/src/Resources/app/storefront/src/main.js ***!
+  \*************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
 
-        eval("//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ2tteS5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6IiIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///Ckmy\n");
+eval("console.info(\"Shop Theme Activated\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ2tteS5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy8vYXBwL2N1c3RvbS9wbHVnaW5zL1NsYWxleWVTaG9wVGhlbWUvc3JjL1Jlc291cmNlcy9hcHAvc3RvcmVmcm9udC9zcmMvbWFpbi5qcz8yZmI1Il0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUuaW5mbyhcIlNob3AgVGhlbWUgQWN0aXZhdGVkXCIpXG4iXSwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///Ckmy\n");
 
-        /***/
-    })
+/***/ })
 
-}, [["Ckmy", "runtime"]]]);
+},[["Ckmy","runtime"]]]);
---
+++ plugins/SlaleyeShopTheme/src/Resources/app/storefront/src/main.js
@@ -0,0 +1,1 @@
+console.info("Shop Theme Activated")
